75dc7a389a07a758ab84fc06d938fcc5b78c89ec
kazedayo <winsonlam@me.com>
2021-07-10 18:15:00 +0800

    update: safe area

--- components/CustomFooter.vue
+++ components/CustomFooter.vue
@@ -1,16 +1,8 @@
 <template>
   <div
-    class="
-      fixed
-      p-4
-      bottom-0
-      inset-x-0
-      w-full
-      backdrop-filter backdrop-blur
-      my-safe
-    "
+    class="fixed p-4 bottom-0 inset-x-0 w-full backdrop-filter backdrop-blur"
   >
-    <p class="text-xs text-center text-gray-400">
+    <p class="text-xs text-center text-gray-400 my-safe">
       Powered by <a href="https://nuxtjs.org">Nuxt.js</a> &
       <a href="https://tailwindcss.com">Tailwind CSS</a>
     </p>
