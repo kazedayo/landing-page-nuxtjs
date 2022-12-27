6486f6f1a919cc5558ba05fcacab473c5fcde980
Winson Lam <winson.lam@keysocapp.com>
2022-12-27 14:49:41 +0800

    fix build error

--- pages/holoschedule/privacypolicy.vue
+++ pages/holoschedule/privacypolicy.vue
@@ -1,17 +1,5 @@
 <template>
   <article class="container mx-auto">
-    <nuxt-content :document="page" />
+    <ContentDoc path="/policy" />
   </article>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  async asyncData({ $content }) {
-    const page = await $content('policy').fetch()
-    return {
-      page,
-    }
-  },
-})
-</script>
